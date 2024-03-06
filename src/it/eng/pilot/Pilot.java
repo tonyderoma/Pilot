c78d445820265401c3bcf4f93beb9b2653e1faf2
antcorin <antonio.corinaldi@eng.it>
2024-03-06 18:24:57 +0100

    toMidnight

--- src/it/eng/pilot/Pilot.java
+++ src/it/eng/pilot/Pilot.java
@@ -3768,6 +3768,10 @@ public class Pilot implements Serializable {
 	}
 
 	private boolean isObj(Object campo, Object... value) {
+		if (campo instanceof Date) {
+			campo = toMidnight((Date) campo);
+		}
+
 		boolean trovato = false;
 		if (null != value) {
 			for (Object el : value) {
@@ -3775,11 +3779,33 @@ public class Pilot implements Serializable {
 					if (Null(el))
 						return true;
 				} else {
+					if (el instanceof Date && campo instanceof Date) {
+						el = toMidnight((Date) el);
+					}
 					trovato = almenoUna(trovato, campo.equals(el));
 				}
 			}
 		}
 		return trovato;
+	}
+
+	/**
+	 * Da una data ritorna un nuovo oggetto Date impostato alla mezzanotte dello
+	 * stesso giorno
+	 * 
+	 * @param d
+	 * @return Date
+	 */
+	private Date toMidnight(Date d) {
+		if (Null(d))
+			return d;
+		Calendar c = Calendar.getInstance();
+		c.setTime(d);
+		c.set(Calendar.HOUR_OF_DAY, 0);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		c.set(Calendar.MILLISECOND, 0);
+		return c.getTime();
 	}
 
 	/**
