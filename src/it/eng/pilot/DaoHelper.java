6e6a2a98eb35a6e46deeaad6473944a2a4d48090
antcorin <antonio.corinaldi@eng.it>
2024-03-29 15:12:55 +0100

    resa meno stringente la where condition su impact() del DaoHelper, tolto byUser(getCodUtente()) anche su altro metodo in overoload

--- src/it/eng/pilot/DaoHelper.java
+++ src/it/eng/pilot/DaoHelper.java
@@ -3300,9 +3300,8 @@ public class DaoHelper extends PilotSupport {
 	 * avuto per la tabella identificata da alias. I record estratti e scritti
 	 * direttamente su file sono quelli modificati nell'intervallo temporale che
 	 * va tra l'istanziazione del dao e l'istante in cui si richiama il metodo
-	 * stesso, modificati dall'utente codUtente e dal codice applicazione
-	 * codAppl entrambi attributi di @DaoConfig. Ritorna il nome del file di
-	 * impatto creato.
+	 * stesso, modificati dal codice applicazione codAppl attributo di
+	 * DaoConfig. Ritorna il nome del file di impatto creato.
 	 * 
 	 * @param alias
 	 * @param path
@@ -3312,7 +3311,7 @@ public class DaoHelper extends PilotSupport {
 	public String impact(String alias, String path) throws Exception {
 		if (tutte(isDsMode(), !isFromBatchCall()))
 			throw new Exception("Modalità EJB in ambiente WEB. Generazione dei file di impatto disabilitata");
-		return giveMe(alias).modifiedBetween(getStart(), now()).byApp(getCodAppl()).byUser(getCodUtente()).selectForWrite(path);
+		return giveMe(alias).modifiedBetween(getStart(), now()).byApp(getCodAppl()).selectForWrite(path);
 	}
 
 	/**
