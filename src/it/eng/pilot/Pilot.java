ec3fd88aab0d27a93c68a729e6898617a7c319f0
antcorin <antonio.corinaldi@eng.it>
2024-10-01 09:41:00 +0200

    plLimit

--- src/it/eng/pilot/Pilot.java
+++ src/it/eng/pilot/Pilot.java
@@ -7247,7 +7247,7 @@ public class Pilot implements Serializable {
 	 * @param limite
 	 * @return PList<K>
 	 */
-	public <K> PList<K> pl(Integer limite) {
+	public <K> PList<K> plLimit(Integer limite) {
 		return getPList(limite);
 	}
 
--- src/it/eng/pilot/PilotSupport.java
+++ src/it/eng/pilot/PilotSupport.java
@@ -744,7 +744,7 @@ public class PilotSupport {
 	 * @param limite
 	 * @return PList<K>
 	 */
-	protected <K> PList<K> pl(Integer limite) {
+	protected <K> PList<K> plLimit(Integer limite) {
 		return getPList(limite);
 	}
 
