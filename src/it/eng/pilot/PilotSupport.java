97914f49ca3f669f54d958a54ff15f7f19c35c4e
antcorin <antonio.corinaldi@eng.it>
2024-10-03 12:58:43 +0200

    metodi per stringhe valute

--- src/it/eng/pilot/PilotSupport.java
+++ src/it/eng/pilot/PilotSupport.java
@@ -1735,4 +1735,49 @@ public class PilotSupport {
 	protected void svuotaFile(File... f) {
 		p.svuotaFile(f);
 	}
+
+	/**
+	 * Ritorna la stringa "€"
+	 * 
+	 * @return String
+	 */
+	protected String euro() {
+		return p.euro();
+	}
+
+	/**
+	 * Ritorna la stringa "$"
+	 * 
+	 * @return String
+	 */
+	protected String dollar() {
+		return "$";
+	}
+
+	/**
+	 * Ritorna la stringa "£"
+	 * 
+	 * @return String
+	 */
+	protected String pound() {
+		return "£";
+	}
+
+	/**
+	 * Ritorna la stringa "*"
+	 * 
+	 * @return String
+	 */
+	protected String star() {
+		return "*";
+	}
+
+	/**
+	 * Ritorna la stringa "~"
+	 * 
+	 * @return String
+	 */
+	protected String tilde() {
+		return "~";
+	}
 }
