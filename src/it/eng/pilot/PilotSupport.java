56fb8fdc6aa6b598412fd8e7ffeaeeb3130dab28
antcorin <antonio.corinaldi@eng.it>
2024-10-04 12:20:19 +0200

    color testo

--- src/it/eng/pilot/PilotSupport.java
+++ src/it/eng/pilot/PilotSupport.java
@@ -1811,4 +1811,19 @@ public class PilotSupport {
 		return p.moneyPound(bd);
 	}
 
+	/**
+	 * Ritorna la stringa s con il colore e gli effetti voluti
+	 * 
+	 * @param s
+	 * @param c
+	 * @param grassetto
+	 * @param corsivo
+	 * @param underscore
+	 * @param barrato
+	 * @return String
+	 */
+	protected String color(String s, Color c, boolean grassetto, boolean corsivo, boolean underscore, boolean barrato) {
+		return p.color(s, c, grassetto, corsivo, underscore, barrato);
+	}
+
 }
